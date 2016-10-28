eb1fcc2f0479f1bceda377018673bcf70688194b
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-10-28 10:05:49 -0700

    allow answers to be deleted

--- app/routes/question.js
+++ app/routes/question.js
@@ -13,6 +13,10 @@ export default Ember.Route.extend({
           return question.save();
         });
         this.transitionTo('question', question);
-      }
+      },
+        destroyAnswer(answer){
+          answer.destroyRecord();
+          this.transitionTo('index');
+        }
     }
 });
