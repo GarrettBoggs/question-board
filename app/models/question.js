70a9aabaee370532f518dbcb5b20d7fbc7d36f31
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-10-28 08:36:35 -0700

    add answers to front page

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   content: DS.attr(),
-  notes: DS.attr()
+  notes: DS.attr(),
+  answer: DS.belongsTo('question', {async: true}),
+  
 });
