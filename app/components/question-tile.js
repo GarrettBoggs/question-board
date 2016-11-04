31eaf3f44a711b40c3569d7c1cdb461395424ec1
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-11-04 09:34:52 -0700

    add favorite list using a service

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
+
   actions: {
     update(question, params) {
      this.sendAction('update', question, params);
@@ -9,6 +11,9 @@ export default Ember.Component.extend({
       if(confirm('Are you sure you want to delete this Question?')) {
         this.sendAction('destroyQuestion', question);
       }
-    }
+    },
+    addFavorite(question){
+      this.get('favoriteQuestions').add(question);
+    },
   }
 });
